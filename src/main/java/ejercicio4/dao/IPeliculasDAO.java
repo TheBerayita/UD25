aba0d823c7c4c4a536e8fc98a687e0bd95dbae8d
Adrytan710 <55663994+Adrytan710@users.noreply.github.com>
2022-02-28 13:26:01 +0100

    Delete src directory

--- src/main/java/ejercicio4/dao/IPeliculasDAO.java
+++
@@ -1,9 +0,0 @@
-package ejercicio4.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ejercicio4.dto.Peliculas;
-
-public interface IPeliculasDAO extends JpaRepository<Peliculas, Integer>{
-
-}
